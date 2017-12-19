0bd43d461026872a91a188cd44d86f95882a1446
LSChyi <alan81920@gmail.com>
2017-12-19 22:25:35 +0800

    add basic check script

---
+++ check_update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+TRIGGER_INTERVAL=7 # unit: days
+
+if [ `uname` = "Darwin" ]; then
+    current_epoch=$(date -j -f "%Y-%m-%d" `date +"%Y-%m-%d"` "+%s")
+    last_epoch=$(date -j -f "%Y-%m-%d" `cat update_date` "+%s")
+else
+    echo "Your system is currently not supported"
+    return 0
+fi
+
+interval_epoch=$(($current_epoch-$last_epoch))
+interval_days=$(($interval_epoch/60/60/24))
+if [ $interval_days -ge $TRIGGER_INTERVAL ]; then
+    date +"%Y-%m-%d" > update_date
+    ./prompt_script.sh
+fi
